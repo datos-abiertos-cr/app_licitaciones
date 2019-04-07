39514607ab1fe82fd417b4436ef0ddab7728015e
ronny <ronny@ixpantia.com>
2019-04-06 20:39:25 -0600

    Ref #1 estructura basica aplicacion

--- server.R
+++ server.R
@@ -1,1 +1,7 @@
 # server
+
+server <- function(input, output) {
+  output$grafico <- renderTable({
+    
+  })
+}
--- ui.R
+++ ui.R
@@ -1,1 +1,45 @@
 # ui.R
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(feather)
+
+# Encabezado --------------------------------------------------------------
+header <- dashboardHeader(
+  title = "Licitaciones adjudicadas 2014-2015"
+)
+
+
+# Sidebar -----------------------------------------------------------------
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Resumen General", tabName = "resumen_general", icon = icon("suitcase"))
+  )
+)
+
+# Cuerpo ------------------------------------------------------------------
+body <- dashboardBody({
+  tabItems(
+    tabItem(
+      tabName = "resumen_general",
+      fluidRow(
+        box(width = 6,
+            title = "Cuadro mayores instituciones  con adjudicaciones",
+            status = "danger",
+            tableOutput("instituciones_adjudicacion"))
+      )
+    )
+  )
+})
+
+# App completo ------------------------------------------------------------
+dashboardPage(
+  skin = "blue",
+  header,
+  sidebar,
+  body
+)
+
+
+
